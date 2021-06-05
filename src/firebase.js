7483f8a35cbc53e6a09d1077c91523b27c455f42
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-06-05 13:04:24 +0530

    Initail commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBfRJ7iVZ-xH_1sCGGWEZoWwziES17D1JQ",
+    authDomain: "slack-clone-5c838.firebaseapp.com",
+    projectId: "slack-clone-5c838",
+    storageBucket: "slack-clone-5c838.appspot.com",
+    messagingSenderId: "529947455483",
+    appId: "1:529947455483:web:078d0efb47660ac6cbe55e",
+    measurementId: "G-DD8M781C0E"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export default db;
