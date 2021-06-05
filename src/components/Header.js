45e775d9225ee60eb3c7d085c60156ec3ff38e0f
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-06-05 13:17:20 +0530

    Changes made

---
+++ src/components/Header.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import styled from 'styled-components'
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
+function Header() {
+    return (
+        <Container>
+            <Main>
+                <AccessTimeIcon />
+                <SearchContainer>
+                    <Search>
+                        <input type="text" placeholder="Search..."></input>
+                    </Search>
+                </SearchContainer>
+                <HelpOutlineIcon />
+            </Main>
+
+            <UserContainer>
+                <Name>
+                    Soumili
+                </Name>
+
+                <UserImage>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiJ9em7-XCwO8MCDSPcRp6IQuaE8Azso7pYQ&usqp=CAU" alt="" width="20px" height="20px" />
+                </UserImage>
+            </UserContainer>
+        </Container>
+    )
+}
+
+export default Header
+
+
+const Container = styled.div`
+    background: #350d36;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    box-shadow: 0 1px 0 0 rgb(255 255 255 / 10%);
+`
+
+const Main = styled.div`
+    display: flex;
+    margin-left: 16px;
+    margin-right: 16px;
+`
+
+const UserContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding-right: 16px;
+    position: absolute;
+    right: 0;
+`
+
+const SearchContainer = styled.div`
+    min-width: 400px;
+    margin-left: 16px;
+    margin-right: 16px;
+`
+
+const Search = styled.div`
+    width: 100%;
+    border-radius: 6px;
+    box-shadow: inset 0 0 0 1px rgb(104 74 104);
+    display: flex;
+    align-items: center;
+
+    input{
+        background-color: transparent;
+        border: none;
+        padding-left: 8px;
+        padding-right: 8px;
+        padding-top: 4px;
+        padding-bottom: 4px;
+        color: white;
+    }
+
+    input:focus{
+        outline: none;
+    }
+`
+
+const Name = styled.div`
+    padding-right: 16px;
+`
+
+const UserImage = styled.div`
+    width: 28px;
+    height: 28px;
+    border: 2px solid white;
+    border-radius: 3px;
+
+    img{
+        width: 100%;
+    }
+`
