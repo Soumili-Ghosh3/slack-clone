64c7dfb4b113e05b737019a428aaf65c447abf1c
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-06-07 10:09:23 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import db from './firebase'
 function App() {
 
   const [rooms, setRooms] = useState([])
+  const [user, setUser] = useState()
 
   const getChannels = () => {
     db.collection('rooms').onSnapshot((snapshot) => {
@@ -27,21 +28,27 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Container>
-          <Header />
-          <Main>
-            <Sidebar rooms={rooms} />
-            <Switch>
-              <Route path="/room">
-                <Chat />
-              </Route>
+        {
+          !user ?
+          <Login />
+          :
+  
+          <Container>
+            <Header />
+            <Main>
+              <Sidebar rooms={rooms} />
+              <Switch>
+                <Route path="/room">
+                  <Chat />
+                </Route>
 
-              <Route path="/">
-                <Login />
-              </Route>
-            </Switch>
-          </Main>
-        </Container>
+                <Route path="/">
+                  <Login />
+                </Route>
+              </Switch>
+            </Main>
+          </Container>
+        }
       </Router>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,54 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Login() {
     return (
-        <div>
-            Login
-        </div>
+        <Container>
+            <Content>
+                <SlackImg src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png" />
+                <h1> Sign In Slack </h1>
+                <SignInButton>
+                    Sign In with Google
+                </SignInButton>
+            </Content>
+        </Container>
     )
 }
 
 export default Login
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #f8f8f8;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Content = styled.div`
+    background: white;
+    padding: 100px;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 1px 3px rgb(0 0 0 / 12%), 0 1px 2px rgb(0 0 0 / 24%);
+
+`
+
+const SlackImg = styled.img`
+    height: 100px;
+`
+
+const SignInButton = styled.button`
+    margin-top: 50px;
+    background-color: #0a8d48;
+    color: white;
+    border: none;
+    height: 40px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 15px;
+`
